856b662a129c0a76a4cb4e02026d4257b12bb489
IamErol <yildirimerol7.0.0@gmail.com>
2023-03-17 00:10:25 +0500

    env update

--- devsearch/devsearch/settings.py
+++ devsearch/devsearch/settings.py
@@ -139,8 +139,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 's.munez700@gmail.com'
-EMAIL_HOST_PASSWORD = 'bqefeflaeqfsotnj'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_TIMEOUT = None
 
 
